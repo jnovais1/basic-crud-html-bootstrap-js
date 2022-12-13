a3a041f3ec99929732f4b7b954c1d0d29db7b787
jnovais1 <jnovais@findes.org.br>
2022-12-13 00:47:07 -0300

    explaining the script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*const modal = document.querySelector('.modal-container')
+const modal = document.querySelector('.modal-container')
 const tbody = document.querySelector('tbody')
 const sNome = document.querySelector('#m-nome')
 const sIdade = document.querySelector('#m-idade')
@@ -8,22 +8,29 @@ const saveButton = document.querySelector('#btnSalvar')
 let pessoas
 let id_pessoa
 
-function getPessoas() {
+//Carrega os itens do 'banco'
+function getPessoas() {  //caso nao tenha nada, retorna um array vazio
     JSON.parse(localStorage.getItem('dbfunc')) ?? []
 }
 
+//Post
 function setPessoas() {
     localStorage.setItem('dbfunc', JSON.stringify(pessoas))
 }
 
+//Get todos os usuários página inicial
 function index() {
     pessoas = getPessoas()
+    //Inicializar html vazio para incrementar depois
     tbody.innerHTML = ''
     pessoas.forEach((pessoa, index) => {
         insertItem(pessoa, index)
     })
-}*/
+}
 
+
+/*
 let nome = document.getElementById("nome")
 let idade = document.getElementById("idade")
 let cpf = document.getElementById("cpf")
+*/
